1ec0acad1b4b2f8b40579aa4978acbaf5d9945f4
JesusGarciaShakur <jesusrguez970@gmail.com>
2023-10-12 12:25:19 -0600

    Using the files package

--- src/ManipulacionArchivos.java
+++ src/ManipulacionArchivos.java
@@ -1,203 +1,13 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
+import files.Files;
 
 public class ManipulacionArchivos {
 
-    public static int countFileLine(String name) {
-        File archivo;
-        FileReader reader;
-        BufferedReader bufer;
-
-        int numLinea = 0;
-
-        try {
-            archivo = new File("C:\\archivos\\" + name + ".txt");
-            reader = new FileReader(archivo);
-            bufer = new BufferedReader(reader);
-
-            while ((bufer.readLine()) != null) {
-                numLinea++;
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo" + e.toString());
-        }
-
-        return numLinea;
-    }
-
-    public static String[] fileToStringArray(String name) throws IOException {
-        File archivo;
-        FileReader reader;
-        BufferedReader bufer;
-        String linea;
-        String[] array = null;
-        int t;
-        int i = 0;
-
-        try {
-            t = countFileLine(name);
-            array = new String[t];
-
-            archivo = new File("C:\\archivos\\" + name + ".txt");
-            reader = new FileReader(archivo);
-            bufer = new BufferedReader(reader);
-
-            while ((linea = bufer.readLine()) != null) {
-                array[i] = linea;
-                i++;
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo" + e.toString());
-        }
-
-        return array;
-    }
-
-    public static int[] fileToIntArray(String name) throws IOException {
-        File archivo;
-        FileReader reader;
-        BufferedReader bufer;
-        String linea;
-        int[] array = null;
-        int t;
-        int i = 0;
-
-        try {
-            t = countFileLine(name);
-            array = new int[t];
-
-            archivo = new File("C:\\archivos\\" + name + ".txt");
-            reader = new FileReader(archivo);
-            bufer = new BufferedReader(reader);
-
-            while ((linea = bufer.readLine()) != null) {
-                array[i] = Integer.parseInt(linea);
-                i++;
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo" + e.toString());
-        }
-
-        return array;
-    }
-
-    public static double[] fileToDoubleArray(String name) throws IOException {
-        File archivo;
-        FileReader reader;
-        BufferedReader bufer;
-        String linea;
-        double[] array = null;
-        int t;
-        int i = 0;
-
-        try {
-            t = countFileLine(name);
-            array = new double[t];
-
-            archivo = new File("C:\\archivos\\" + name + ".txt");
-            reader = new FileReader(archivo);
-            bufer = new BufferedReader(reader);
-
-            while ((linea = bufer.readLine()) != null) {
-                array[i] = Double.parseDouble(linea);
-                i++;
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo" + e.toString());
-        }
-
-        return array;
-    }
-
-    public static boolean[] fileToBooleanArray(String name) throws IOException {
-        File archivo;
-        FileReader reader;
-        BufferedReader bufer;
-        String linea;
-        boolean[] array = null;
-        int t;
-        int i = 0;
-
-        try {
-            t = countFileLine(name);
-            array = new boolean[t];
-
-            archivo = new File("C:\\archivos\\" + name + ".txt");
-            reader = new FileReader(archivo);
-            bufer = new BufferedReader(reader);
-
-            while ((linea = bufer.readLine()) != null) {
-                array[i] = Boolean.parseBoolean(linea);
-                i++;
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo" + e.toString());
-        }
-
-        return array;
-    }
-
-    public static void writeKeyboardToFile(String name) {
-        FileWriter archivo;
-        PrintWriter writer;
-        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
-        String entrada;
-        char respuesta;
-
-        try {
-            archivo = new FileWriter("C:\\archivos\\" + name + ".txt");
-            writer = new PrintWriter(archivo);
-
-            do {
-                System.out.println("Escribe el contenido del archivo");
-                entrada = bufer.readLine();
-                writer.println(entrada);
-
-                System.out.println("Escribe la letra X para detener, cualquier otra tecla llena el archivo");
-                entrada = bufer.readLine();
-                respuesta = entrada.toLowerCase().charAt(0);
-            } while (respuesta != 'x');
-
-            archivo.close();
-        } catch (Exception e) {
-            System.out.println("Error al escribir el archivo" + e.toString());
-        }
-    }
-
-    // Crear un archivo con información de un arreglo
-    public static void writeArrayToFile(String name, int[] array) {
-        FileWriter archivo;
-        PrintWriter writer;
-
-        try {
-            archivo = new FileWriter("C:\\archivos\\" + name + ".txt");
-            writer = new PrintWriter(archivo);
-            // Guardar el arreglo a un archivo
-            for (int unDato : array) {
-                writer.println(unDato);
-            }
-            archivo.close();
-        } catch (Exception e) {
-            System.out.println("Error al escribir el archivo" + e.toString());
-        }
-    }
-
     public static void main(String[] args) throws IOException {
+        Files files = new Files();
         BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
 
         String fileName;
@@ -210,7 +20,7 @@ public class ManipulacionArchivos {
         System.out.println("Escribe el nombre del archivo");
         System.out.println("-----------------------------");
         fileName = bufer.readLine();
-        mascotas = fileToStringArray(fileName);
+        mascotas = files.fileToStringArray(fileName);
         System.out.println("Contenido del arreglo de juegos");
         for (String unlinea : mascotas) {
             System.out.println(unlinea);
@@ -221,7 +31,7 @@ public class ManipulacionArchivos {
         System.out.println("Escribe el nombre del archivo");
         System.out.println("-----------------------------");
         fileName = bufer.readLine();
-        num = fileToIntArray(fileName);
+        num = files.fileToIntArray(fileName);
         System.out.println("Contenido del arreglo de números enteros");
         for (int numi : num) {
             System.out.println(numi);
@@ -257,7 +67,7 @@ public class ManipulacionArchivos {
         }
         System.out.println("Escribe el nombre del archivo de numeros X3: ");
         fileName = bufer.readLine();
-        writeArrayToFile(fileName, numx3);
+        files.writeArrayToFile(fileName, numx3);
 
         // System.out.println("Crear un archivo de texto");
         // System.out.println("-----------------------------");
